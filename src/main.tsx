b0482d54480d6d93167ab8fa39cf801af9b4efbb
Gideon Cameron <gideonwork20@gmail.com>
2025-07-29 01:38:12 -0700

    Updated the main file to use the AuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 // import { ThemeProvider } from "./context/ThemeProvider"; // ❌ Disable for now
 import "./index.css";
+import { AuthProvider } from "./context/AuthContext";
 
 // 👇 Render without ThemeProvider
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
