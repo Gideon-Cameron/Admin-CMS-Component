bce7b32db3134bd578bd73c2046418f49e7bcc7e
Gideon Cameron <gideonwork20@gmail.com>
2025-07-30 03:54:30 -0700

    Moving the main page to home.tsx insted of app.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,27 @@
+import Hero from "../components/Hero";
+// import About from "../components/About";
+// import Experience from "../components/Experience";
+// import Skills from "../components/Skills";
+// import Projects from "../components/Projects";
+import Testimonial from "../components/Testimonial";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <main className="pt-10 space-y-22 px-6 nav:pl-20 md:px-12">
+        <section id="hero"><Hero /></section>
+        {/* <section id="about"><About /></section>
+        <section id="experience"><Experience /></section>
+        <section id="skills"><Skills /></section>
+        <section id="projects"><Projects /></section> */}
+        <section id="testimonials"><Testimonial /></section>
+        <section id="contact"><Contact /></section>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
